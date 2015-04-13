e921abf6b9abddcc8a6cc57ee6b06968cfa4085e
eezhal92 <arkinthesky.69@gmail.com>
2015-04-13 17:42:26 +0700

    add village relation, is_student and full_name accessor

--- app/models/People.php
+++ app/models/People.php
@@ -9,11 +9,26 @@ class People extends Eloquent {
 	 */
 	protected $table = 'people';
 
-    protected $fillable = ['first_name', 'last_name', 'address', 'village_id', 'date_of_birth'];
+    protected $fillable = [
+        'first_name', 'last_name', 'address', 'village_id', 'date_of_birth'
+    ];
     
     public $timestamps = true;
     
     public function student() {
         return $this->hasOne('Student');   
     }
+    
+    public function village() {
+        return $this->belongsTo('Village'); 
+    }
+    
+    public function getIsStudentAttribute() {
+        return ($this->student()->count() >= 1) ? true : false;
+    }
+    
+    public function getFullNameAttribute() {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+    
 }
