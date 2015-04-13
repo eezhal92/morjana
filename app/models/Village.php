58bc62249624976530e407106972099c8dbdb7e7
eezhal92 <arkinthesky.69@gmail.com>
2015-04-13 17:45:35 +0700

    add people relation, stundents function

--- app/models/Village.php
+++ app/models/Village.php
@@ -1,9 +1,30 @@
 <?php 
 
+use \Illuminate\Database\Eloquent\Collection;
+
 class Village extends Eloquent {
     protected $table = 'villages';
     
     public function sub_district() {
         return $this->belongsTo('SubDistrict');
     }
+    
+    public function people() {
+        return $this->hasMany('People');
+    }
+    
+    public function students() {
+        $students = new Collection;
+        
+        $village = $this->load('people.student');
+        
+        foreach($village->people as $people) {    
+            if(!is_null($people->student)) {
+                $students->add($people->student);
+            }
+        }
+        
+        return ($students->count() > 0) ? $students : null;
+    }
+    
 }
