a43822f4de83acfb7eb8ec4a20c74dc7dc6a5d36
eezhal92 <arkinthesky.69@gmail.com>
2015-04-13 22:53:14 +0700

    create remind form view

--- app/views/password/remind.blade.php
+++ app/views/password/remind.blade.php
@@ -1,1 +1,11 @@
-{{ Form::open(['route' => account.remind'']) }}
+@if(Session::has('status'))
+  {{ Session::get('status') }}
+@elseif(Session::get('error'))
+  {{ Session::get('error') }}
+@endif
+   
+
+   {{ Form::open(['route' => 'account.postRemind']) }}
+    {{ Form::text('email', null, ['placeholder' => 'Enter your e-mail here']) }}
+    <input type="submit" value="remind me!">
+{{ Form::close() }}
