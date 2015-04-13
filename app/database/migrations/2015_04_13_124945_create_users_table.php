593968a9b5cc1ee4a85a3e26afd92eee1aeafb1c
eezhal92 <arkinthesky.69@gmail.com>
2015-04-13 20:10:25 +0700

    create users & password reminder migration and reminders controller

---
+++ app/database/migrations/2015_04_13_124945_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('people_id')->unsigned();
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('active', ['yes', 'no']);
+            $table->dateTime('last_login');
+            $table->rememberToken();
+            $table->timestamps();    
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('users');
+	}
+
+}
