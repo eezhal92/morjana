1876b65137df47ef30bcc8d9af97b549c2118019
eezhal92 <arkinthesky.69@gmail.com>
2015-04-14 15:24:37 +0700

    add dummy content

---
+++ app/views/sidenav.blade.php
@@ -0,0 +1,29 @@
+ <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li class="active">
+            <a href="#"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>              
+        <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-user"></i> Students <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="demo" class="collapse">
+                <li>
+                    <a href="#">Index</a>
+                </li>
+                <li>
+                    <a href="#">Add New</a>
+                </li>
+                <li>
+                    <a href="#">Recap</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#"><i class="fa fa-fw fa-bar-chart-o"></i> Statistics</a>
+        </li>
+        <li>
+            <a href="#"><i class="fa fa-fw fa-file"></i> Blank Page</a>
+        </li>
+    </ul>
+</div>
+<!-- /.navbar-collapse -->
---
+++ app/views/students/index.blade.php
@@ -0,0 +1,68 @@
+@extends('master')
+
+
+@section('content')
+    
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                Students <small>master data</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li class="active">
+                    <i class="fa fa-dashboard"></i> Dashboard
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="alert alert-info alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <i class="fa fa-info-circle"></i>  <strong>Like SB Admin?</strong> Try out <a href="http://startbootstrap.com/template-overviews/sb-admin-2" class="alert-link">SB Admin 2</a> for additional features!
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+    
+    <div class="row">
+        <div class="col-lg-12">
+          
+          <table class="table table-bordered table-hover table-striped">
+            <thead>
+              <tr>
+                <th width="100px">Student No.</th>
+                <th>Full Name</th>
+                <th>Gender</th>
+                <th>Faculty</th>
+                <th>Major</th>
+                <th>Year</th>
+                <th width="120px">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+              @foreach($students as $student)
+                  <tr>
+                    <td>{{ $student->student_number }}</td>
+                    <td>{{ $student->people->full_name }}</td>
+                    <td>{{ $student->people->gender }}</td>
+                    <td>{{ $student->major->faculty->name }}</td>
+                    <td>{{ $student->major->name }}</td>
+                    <td>{{ $student->year }}</td>
+                    <td>
+                        <a href="#" class="btn btn-sm btn-warning">Edit</a>
+                        <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                    </td>
+                  </tr>  
+              @endforeach
+              
+            </tbody>
+          </table>
+        
+        </div>
+    </div>
+    
+@stop
