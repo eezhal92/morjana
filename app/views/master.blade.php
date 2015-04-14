34599eafe38a35ca697f855cd380e52065719331
eezhal92 <arkinthesky.69@gmail.com>
2015-04-14 15:23:47 +0700

    remove flash message

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,13 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
     <title>Morowali Sarjana App</title>
+
+    <!-- Bootstrap Core CSS -->
+    {{ HTML::style('css/bootstrap.min.css') }}
+    <!-- Custom CSS -->
+    {{ HTML::style('css/sb-admin.css') }}
+    {{ HTML::style('css/style.css') }}
+
+    <!-- Morris Charts CSS -->
+    {{ HTML::style('css/plugins/morris.css') }}
+    
+    <!-- Custom Fonts -->
+    {{ HTML::style('font-awesome/css/font-awesome.min.css') }}
+    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
+
 <body>
-    <div class="container">
-        @if(Session::has('message')) {{ Session::get('message') }}  @endif
-        @yield('content')
+
+    <div id="wrapper">
+
+        @include('navigation')
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+                
+                @yield('content')
+                
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
     </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    {{ HTML::script('js/jquery.js') }}
+
+    <!-- Bootstrap Core JavaScript -->
+    {{ HTML::script('js/bootstrap.min.js') }}
+
+    <!-- Morris Charts JavaScript -->
+    {{ HTML::script('js/plugins/morris/raphael.min.js') }}
+    {{ HTML::script('js/plugins/morris/morris.min.js') }}
+    {{ HTML::script('js/plugins/morris/morris-data.js') }}
+
 </body>
+
 </html>
