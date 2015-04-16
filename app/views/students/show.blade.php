c7f4f957493357df5e4cc1c4d5b813ece027657c
eezhal92 <arkinthesky.69@gmail.com>
2015-04-16 17:26:56 +0700

    make table for student details

--- app/views/students/show.blade.php
+++ app/views/students/show.blade.php
@@ -24,8 +24,11 @@
     <div class="col-lg-12">
         <div class="row">
             <div class="col-md-6">
-                <table class="table table-hover table-striped">                    
+                <table class="table table-bordered table-hover table-striped">                    
                     <tbody>
+                        <tr class="info">
+                            <td colspan="2" style="text-align:center; font-weight:bold;">Student Details</td>                            
+                        </tr>
                         <tr>
                             <td>Student No.</td>
                             <td>{{ $student->student_number }}</td>
@@ -54,7 +57,70 @@
                             <td>Village</td>
                             <td>{{ $student->people->village->name }}</td>
                         </tr>
+                        <tr>
+                            <td class="info" colspan="2" style="text-align:center; font-weight:bold;">Father</td>
+                        </tr>
+                        <tr>
+                            <td>Full Name</td>
+                            <td>{{ $student->father->full_name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Address</td>
+                            <td>{{ $student->father->address }}</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" class="info" style="text-align:center; font-weight:bold;">Mother</td>
+                        </tr>
+                        <tr>
+                            <td>Full Name</td>
+                            <td>{{ $student->mother->full_name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Address</td>
+                            <td>{{ $student->mother->address }}</td>
+                        </tr>
                         
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-6">
+                <table class="table table-bordered table-hover table-striped">
+                    <tbody>
+                        <tr>
+                            <td colspan="2" class="info" style="text-align:center; font-weight:bold;">Education</td>
+                        </tr>
+                        <tr>
+                            <td>Name</td>
+                            <td>{{ $student->univercity->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Province</td>
+                            <td>{{ $student->univercity->city->province->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>City</td>
+                            <td>{{ $student->univercity->city->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Faculty</td>
+                            <td>{{ $student->major->faculty->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Major</td>
+                            <td>{{ $student->major->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Type</td>
+                            <td>{{ $student->type }}</td>
+                        </tr>
+                        <tr>
+                            <td>Status</td>
+                            <td>{{ $student->status }}</td>
+                        </tr>
+                        <tr>
+                            <td>Amount of Grant</td>
+                            <td>{{ $student->amount_of_grant }}</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
