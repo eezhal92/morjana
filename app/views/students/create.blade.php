fdf7173408c392f8e7a5ab406e62215d1938fb6a
eezhal92 <arkinthesky.69@gmail.com>
2015-04-18 22:02:00 +0700

    add validation message

--- app/views/students/create.blade.php
+++ app/views/students/create.blade.php
@@ -27,6 +27,13 @@
                     <!-- student form -->
                     <div class="col-md-6">
                         <h3>Student Details</h3>
+                        @if($errors->count() > 0)
+                            <div class="alert alert-danger">
+                                {{ var_dump($errors) }}
+                            </div>
+                        @elseif(Session::has('error'))
+                            {{ Session::get('error') }}
+                        @endif
                         
                         <div class="form-group form-group @if($errors->first('student_number')) has-error @endif">
                             <label for="student_number">Student No.</label>
@@ -95,37 +102,56 @@
                            </div>                            
                             <div class="col-md-6">  
                                                            
-                                <div class="form-group">
+                                <div class="form-group @if($errors->first('father_first_name')) has-error @endif">
                                     <label for="father_first_name">Father First Name</label>
                                    {{ Form::text('father_first_name', null, ['class' => 'form-control']) }}
+                                   @if($errors->first('father_first_name'))
+                                    <span class="help-block"> {{ $errors->first('father_first_name') }} </span>
+                                @endif
                                 </div>
                                 
-                                <div class="form-group">
+                                <div class="form-group @if($errors->first('father_last_name')) has-error @endif">
                                     <label for="father_last_name">Father Last Name</label>
                                      {{ Form::text('father_last_name', null, ['class' => 'form-control']) }}
+                                     @if($errors->first('father_last_name'))
+                                        <span class="help-block"> {{ $errors->first('father_last_name') }} </span>
+                                    @endif
+                                     
                                 </div>
                                 
-                                <div class="form-group">
+                                <div class="form-group @if($errors->first('father_address')) has-error @endif">
                                 <label for="father_address">Father Address</label>
                                    {{ Form::textarea('father_address', null, ['class' => 'form-control', 'rows' => 3]) }}
+                                   @if($errors->first('father_address'))
+                                        <span class="help-block"> {{ $errors->first('father_address') }} </span>
+                                    @endif
                                 </div>
                                 
                             </div>
                             <div class="col-md-6">  
                                                            
-                                <div class="form-group">
+                                <div class="form-group @if($errors->first('mother_first_name')) has-error @endif">
                                     <label for="mother_first_name">Mother First Name</label>
                                    {{ Form::text('mother_first_name', null, ['class' => 'form-control']) }}
+                                   @if($errors->first('mother_first_name'))
+                                        <span class="help-block"> {{ $errors->first('mother_first_name') }} </span>
+                                    @endif
                                 </div>
                                 
-                                <div class="form-group">
+                                <div class="form-group @if($errors->first('mother_last_name')) has-error @endif">
                                     <label for="mother_last_name">Mother Last Name</label>
                                      {{ Form::text('mother_last_name', null, ['class' => 'form-control']) }}
+                                     @if($errors->first('mother_last_name'))
+                                        <span class="help-block"> {{ $errors->first('mother_last_name') }} </span>
+                                    @endif
                                 </div>
                                 
-                                <div class="form-group">
+                                <div class="form-group @if($errors->first('mother_address')) has-error @endif">
                                     <label for="mother_address">Mother Address</label>
                                    {{ Form::textarea('mother_address', null, ['class' => 'form-control', 'rows' => 3]) }}
+                                   @if($errors->first('mother_address'))
+                                        <span class="help-block"> {{ $errors->first('mother_address') }} </span>
+                                    @endif
                                 </div>
                             </div>
                         </div>
@@ -134,55 +160,97 @@
                     <!-- univercity form -->
                     <div id="univercityForm" class="col-md-6" style="background: #f5f5f5;">
                        <h3>Univercity / College Details</h3>
-                        <div class="form-group">
-                            <label for="univercity">Univercity / College Name</label>
+                        
+                           <div class="form-group @if($errors->first('univercity')) has-error @endif">
+                            <label for="univercity ">Univercity / College Name</label>
                             {{ Form::text('univercity', null, ['id' => 'univercity', 'class' => 'form-control']) }}
                             <small class="help-block">Tips: Wait a moment for suggestions</small>
+                            @if($errors->first('univercity'))
+                                <span class="help-block"> {{ $errors->first('univercity') }} </span>
+                            @endif
                             <input type="hidden" name="univercity_id" id="univercityId" value="@if(Input::old('univercity_id')){{Input::old('univercity_id')}}@endif">
                         </div>
-                        <div class="form-group">
+                        
+                        <div class="form-group @if($errors->first('univercity_type')) has-error @endif">
                             <label for="type">Public / Private</label>
                             {{ Form::select('univercity_type', $univercity_type_options, null, ['id' => 'univercityType', 'class' => 'form-control univercity-details']) }}
+                            @if($errors->first('univercity_type'))
+                                <span class="help-block"> {{ $errors->first('univercity_type') }} </span>
+                            @endif
                         </div>
-                        <div class="form-group">
+                        
+                        <div class="form-group @if($errors->first('univercity_province_id')) has-error @endif">
                             <label for="univercity_province_id">Province</label>
                             {{ Form::select('univercity_province_id', $provinces, null, ['id' => 'univercityProvinceId', 'class'=> 'form-control univercity-details']) }}
+                            @if($errors->first('univercity_province_id'))
+                                <span class="help-block"> {{ $errors->first('univercity_province_id') }} </span>
+                            @endif
                         </div>
-                        <div class="form-group">
+                        
+                        <div class="form-group @if($errors->first('univercity_city_id')) has-error @endif">
                             <label for="univercity_city_id">City</label>
                             <select  name="univercity_city_id" id="univercityCityId" class="form-control univercity-details" data-id="@if(Input::old('univercity_city_id')){{Input::old('univercity_city_id')}}@endif">
                             <option value="">Please select province first</option>
                             </select>
+                            @if($errors->first('univercity_city_id'))
+                                <span class="help-block"> {{ $errors->first('univercity_city_id') }} </span>
+                            @endif
                         </div>
-                        <div class="form-group">
+                        
+                        <div class="form-group @if($errors->first('faculty')) has-error @endif">
                             <label for="faculty">Faculty</label>
                             <input type="text" name="faculty" id="faculty" class="form-control">
                             <small class="help-block">Tips: Wait a moment for suggestions</small>
+                            @if($errors->first('faculty'))
+                                <span class="help-block"> {{ $errors->first('faculty') }} </span>
+                            @endif
                             <input type="hidden" name="faculty_id" value="">
                         </div>
-                        <div class="form-group">
+                        
+                        <div class="form-group @if($errors->first('major')) has-error @endif">
                             <label for="major">Major</label>
                             <input type="text" name="major" id="major" class="form-control">
                             <small class="help-block">Tips: Wait a moment for suggestions</small>
+                            @if($errors->first('major'))
+                                <span class="help-block"> {{ $errors->first('major') }} </span>
+                            @endif
                             <input type="hidden" name="major_id" value="">
                         </div>
-                        <div class="form-group">
+                        
+                        <div class="form-group @if($errors->first('degree_id')) has-error @endif">
                             <label for="degree_id">Degree</label>                            
                             {{ Form::select('degree_id', $degrees, null, ['class' => 'form-control']) }}
+                            @if($errors->first('degree_id'))
+                                <span class="help-block"> {{ $errors->first('degree_id') }} </span>
+                            @endif
                         </div>
-                        <div class="form-group">
+                        
+                        <div class="form-group @if($errors->first('year')) has-error @endif">
                             <label for="year">Year</label>
                             {{ Form::text('year', null, ['class' => 'form-control']) }}
+                            @if($errors->first('year'))
+                                <span class="help-block"> {{ $errors->first('year') }} </span>
+                            @endif
                         </div>
+                        
                         <div class="form-group">
-                            <label for="student_type">Type</label>
-                            {{ Form::select('student_type', ['regular' => 'Regular', 'non_regular' => 'Non-Regular'], null, ['id' => 'subDistrict', 'class'=> 'form-control']) }}
+                            <label for="student_type  @if($errors->first('type')) has-error @endif">Type</label>
+                            {{ Form::select('type', ['regular' => 'Regular', 'non_regular' => 'Non-Regular'], null, ['class'=> 'form-control']) }}
+                            @if($errors->first('type'))
+                                <span class="help-block"> {{ $errors->first('type') }} </span>
+                            @endif
                         </div>
                         
                         <h3>Other</h3>
-                        <div class="form-group input-group">
+                        <div class="form-group input-group @if($errors->first('amount_of_grant')) has-error @endif">
                             <span class="input-group-addon">Rp</span>
                             {{ Form::text('amount_of_grant', null, ['class' => 'form-control', 'placeholder' => 'Amount of grant']) }}
+                            
+                        </div>
+                        <div class="form-group @if($errors->first('amount_of_grant')) has-error @endif">
+                            @if($errors->first('amount_of_grant'))
+                                <p class="help-block"> {{ $errors->first('amount_of_grant') }} </p>
+                            @endif
                         </div>
                         
                         <button type="submit" class="btn btn-primary btn-block">Save</button>
@@ -233,10 +301,24 @@
             return villages;
         }
         
-//        $('#subDistrict').on('change', function() {
-//            $('#village').attr('disabled', true);
-//            getVillages($(this).val());
-//        });
+        $('#subDistrict').on('change', function(evt, data) {           
+            console.log('Triggered change on subDistrict...');
+            $('#villageId').attr('disabled', true);
+            $.when(getVillages($(this).val())).done(function(villages) {
+//                console.log(data.village_id);
+                appendToSelect('#villageId', villages);
+                $('#villageId').attr('disabled', false);
+                if(typeof data !== 'undefined') {
+                    $('#villageId').val(data.village_id);
+                }
+            });
+        });
+        
+        if($('#villageId').data('id') !== '' || $('#subDistrict').val() !== '') {
+            var village_data_id = $('#villageId').data('id');
+            console.log(village_data_id);
+            $('#subDistrict').trigger('change', [{village_id: village_data_id}]);
+        }
         
         /* Faculty and Majors */
         
@@ -293,7 +375,7 @@
                     $('#univercityCityId').val(data.city_id);
                     
                     if($('#univercityId').val() !== '') {
-                        alert($('#univercityId').val());
+//                        alert($('#univercityId').val());
                         $('.univercity-details').attr('disabled', true);
                     }  else{
                         $('.univercity-details').attr('disabled', false);
@@ -310,27 +392,6 @@
             var city_data_id = $('#univercityCityId').data('id');
             console.log(city_data_id);
             $('#univercityProvinceId').trigger('change', [{city_id: city_data_id}]);
-        }
-        
-        $('#subDistrict').on('change', function(evt, data) {           
-            console.log('Triggered change on subDistrict...');
-            $('#villageId').attr('disabled', true);
-            $.when(getVillages($(this).val())).done(function(villages) {
-//                console.log(data.village_id);
-                appendToSelect('#villageId', villages);
-                $('#villageId').attr('disabled', false);
-                if(typeof data !== 'undefined') {
-                    $('#villageId').val(data.village_id);
-                }
-            });
-        });
-        
-//        $('#subDistrict').trigger('change');
-        
-        if($('#villageId').data('id') !== '' || $('#subDistrict').val() !== '') {
-            var village_data_id = $('#villageId').data('id');
-            console.log(village_data_id);
-            $('#subDistrict').trigger('change', [{village_id: village_data_id}]);
         }
         
         $('#univercity').devbridgeAutocomplete({
